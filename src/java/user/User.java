350308472e49769a0449682e45073b9af1285eec
quangtmhe130060 <quangtmhe130060@fpt.edu.vn>
2019-03-05 11:14:05 +0700

    commited user and dao

--- src/java/user/User.java
+++ src/java/user/User.java
@@ -1,33 +1,55 @@
-package user;
-
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
+package loadimage;
 
 /**
  *
- * @author Kiruu
+ * @author LEGION
  */
 public class User {
-    private String userName;
+
+    private String id;
+    private String name;
+    private String nickname;
     private String password;
+    private byte[] avatar;
 
     public User() {
     }
 
-    public User(String userName, String password) {
-        this.userName = userName;
+    public User(String id, String name, String nickName, String password, byte[] image) {
+        this.id = id;
+        this.name = name;
+        this.nickname = nickName;
         this.password = password;
+        this.avatar = image;
     }
 
-    public String getUserName() {
-        return userName;
+    public String getId() {
+        return id;
     }
 
-    public void setUserName(String userName) {
-        this.userName = userName;
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getNickName() {
+        return nickname;
+    }
+
+    public void setNickName(String nickName) {
+        this.nickname = nickName;
     }
 
     public String getPassword() {
@@ -38,6 +60,21 @@ public class User {
         this.password = password;
     }
 
-    
-    
+    public byte[] getImage() {
+        return avatar;
+    }
+
+    public void setImage(byte[] image) {
+        this.avatar = image;
+    }
+
+    @Override
+    public String toString() {
+        String s = "";
+        for (byte b : avatar) {
+            s += b + ",";
+        }
+        return s;
+    }
+
 }
